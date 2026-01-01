81094a86ecca300b5aa340199725403fa1ac86b7
calebrom5774 <calebrom5774@gmail.com>
2026-01-01 14:11:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'jimina-v4';
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./index.html', './manifest.json', './icon.png']);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
