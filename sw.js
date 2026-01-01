bbe0099b815dfcf4ce17945da6e6ceb4f4e9c6e3
calebrom5774 <calebrom5774@gmail.com>
2026-01-01 14:06:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
-  console.log('吉米娜：神域服務已安裝');
+  self.skipWaiting();
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
-
-}); 
+});
